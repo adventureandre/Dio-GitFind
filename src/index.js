85c3870cf3b27c65e9f624bf0d0602e458dada51
Andre Luiz <adventureandre@hotmail.com>
2024-02-18 20:40:55 -0300

    Update the code Fixes #1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css'
 import App from './pages/Home';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queyClient } from './lib/reac-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queyClient}>
+      <App />
+      </QueryClientProvider>
+    
   </React.StrictMode>
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,71 +1,108 @@
-import {useState} from "react";
-import {Header} from "../../components/Header";
-import background from '../../assets/background.png'
+import React, { useState } from "react";
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
 import ItemList from "../../components/ItemList";
-import "./styles.css"
+import "./styles.css";
+import { useQuery } from "@tanstack/react-query";
+
+
+
+function fetchUserData(username) {
+  return fetch(`https://api.github.com/users/${username}`).then((response) => {
+    if (!response.ok) {
+      throw new Error("Erro ao carregar dados do usuário");
+    }
+    return response.json();
+  });
+}
 
 function App() {
-    const [user, setUser] = useState('');
-    const [currentUser, setCurrentUser] = useState(null);
-    const [repos, setRepos] = useState(null);
+  const [user, setUser] = useState("");
+  const [isSubmiting, setIsSubmiting] = useState(false)
 
-    const handleGetData = async () => {
-        const userData = await fetch(`https://api.github.com/users/${user}`);
-        const newUser = await userData.json();
+  const {
+    data: currentUser,
+    isLoading: userIsLoading,
+    isError: userIsError,
+  } = useQuery({
+    queryKey: ["userData", user],
+    queryFn: () => fetchUserData(user),
+    enabled: !!user && isSubmiting,
+  });
 
-        if (newUser.name) {
-            const {avatar_url, name, bio, login} = newUser
-            setCurrentUser({avatar_url, name, bio,login})
-
-            const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
-            const newRepos = await reposData.json();
-            if (newRepos.length) {
-                setRepos(newRepos);
-            }
+  const {
+    data: repos,
+    isLoading: reposIsLoading,
+    isError: reposIsError,
+  } = useQuery({
+    queryKey: ["userRepos", user],
+    queryFn: () =>
+      fetchUserData(user).then((userData) => {
+        if (userData.repos_url) {
+          return fetch(userData.repos_url).then((response) => response.json());
         }
-    }
+        return [];
+      }),
 
-    return (
-        <div className="App">
-            <Header/>
-            <div className="conteudo">
-                <img src={background} className='background' alt="imagen"/>
-                <div className='info'>
-                    <div>
-                        <input
-                            name="usuario"
-                            value={user}
-                            onChange={event => setUser(event.target.value)}
-                            placeholder='@username'/>
-                        <button onClick={handleGetData}>Buscar</button>
-                    </div>
-                    {currentUser?.name ? (
-                        <>
-                            <div className='perfil'>
-                                <img src={currentUser.avatar_url} className='profile'
-                                     alt={currentUser.name}/>
-                                <div>
-                                    <h3>{currentUser.name}</h3>
-                                    <span>@{currentUser.login}</span>
-                                    <p>{currentUser.bio}</p>
-                                </div>
-                            </div>
-                            <hr/>
-                        </>
+    enabled: !!user && !!currentUser?.repos_url && isSubmiting,
+  });
 
-                    ) : null}
-                    {repos?.length ? (
-                    <div>
-                        <h4 className='repositorio'>Repositorios</h4>
-                        {repos.slice(0,5).map(repo =>(
-                            <ItemList title={repo.name} description={repo.description}/>
-                        ))}
-                    </div>
-                        ):null}
+  const handleGetData = () => {
+    setIsSubmiting(true)
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="imagen" />
+        <div className="info">
+          <div>
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
+            />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {userIsError && <div>Erro ao carregar dados do usuário</div>}
+          {userIsLoading && <div>Carregando dados do usuário...</div>}
+          {currentUser && (
+            <>
+              <div className="perfil">
+                <img
+                  src={currentUser.avatar_url}
+                  className="profile"
+                  alt={currentUser.name}
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
                 </div>
+              </div>
+              <hr />
+            </>
+          )}
+          {reposIsError && <div>Erro ao carregar repositórios do usuário</div>}
+          {reposIsLoading && <div>Carregando repositórios do usuário...</div>}
+          {repos && repos.length > 0 && (
+            <div>
+              <h4 className="repositorio">Repositórios</h4>
+              {repos.slice(0, 5).map((repo) => (
+                <ItemList
+                  key={repo.id}
+                  title={repo.name}
+                  description={repo.description}
+                />
+              ))}
             </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
